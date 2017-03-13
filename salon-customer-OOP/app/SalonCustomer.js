368083c36c04342eba460b3ba39add9cbd2adf24
Oahray <rodneyoare@gmail.com>
2017-03-13 12:50:46 +0000

    Added a Salon Customer OOP implementation

---
+++ salon-customer-OOP/app/SalonCustomer.js
@@ -0,0 +1,48 @@
+var SalonCustomer = function(age) {
+	this.age = age;
+	this.numOfHairCuts = 0;
+
+	this.rate;
+	if (this.age === 'child') {
+		this.rate = 200;
+	}
+	else {
+		this.rate = 300;
+	}
+
+	this.getFee = function() {
+		var fee = this.rate * this.numOfHairCuts;
+		return fee;
+	}
+
+	this.fee = this.getFee();
+
+	this.getHairCut = function() {
+		this.numOfHairCuts += 1;
+		this.fee = this.getFee()
+	};
+
+	this.reset = function() {
+		this.numOfHairCuts = 0;
+		return this.numOfHairCuts;
+	}
+
+	this.pay = function(cash) {
+		this.cash = cash;
+		this.fee = this.getFee();
+		if (this.cash < this.fee) {
+			return "Your money no complete, bros";
+		}
+		else if (this.cash === this.fee) {
+			this.numOfHairCuts = this.reset();
+			return "Thank you";
+		}
+		else {
+			var change = (this.cash - this.fee).toString();
+			this.numOfHairCuts = this.reset();
+			return "Thank you. take your " + change + " Naira change";
+		}
+	}
+}
+
+module.exports = SalonCustomer;
